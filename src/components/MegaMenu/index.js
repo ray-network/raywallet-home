6fc93816d98d0ba2bb3d0282d75f79ad79ed9716
Ray Robot <hello@rraayy.com>
2022-01-15 16:51:41 +0200

    update

--- src/components/MegaMenu/index.js
+++ src/components/MegaMenu/index.js
@@ -62,7 +62,7 @@ const MegaMenu = () => {
                   >
                     <div className="d-flex mb-4">
                       <h6 className="mb-0">
-                        <strong>Information</strong>
+                        <strong>Resources</strong>
                       </h6>
                     </div>
                     <ul className="list-unstyled mb-5">
@@ -113,8 +113,9 @@ const MegaMenu = () => {
                         </a>
                       </li>
                       <li>
-                        <a href="https://docs.raynetwork.io/">
-                          Documentation <i className="ri ri-arrow_up ri-ext" />
+                        <a href="https://raynetwork.io/terms-of-use/">
+                          Terms of Use
+                          <i className="ri ri-arrow_up ri-ext" />
                         </a>
                       </li>
                       <li>
@@ -124,9 +125,13 @@ const MegaMenu = () => {
                         </a>
                       </li>
                       <li>
-                        <a href="https://raynetwork.io/terms-of-use/">
-                          Terms of Use
-                          <i className="ri ri-arrow_up ri-ext" />
+                        <a href="https://docs.raynetwork.io/">
+                          Documentation <i className="ri ri-arrow_up ri-ext" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://github.com/ray-network/">
+                          GitHub <i className="ri ri-arrow_up ri-ext" />
                         </a>
                       </li>
                     </ul>
